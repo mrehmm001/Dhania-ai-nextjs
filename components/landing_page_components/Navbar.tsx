c8fd4d512e7fbe7064bac338dc09d797ab071ed7
mrehmm001 <muneeb73@outlook.com>
2023-11-26 23:22:24 +0000

    [BUGFIX] fix distorted logo when in mobile view

--- components/landing_page_components/Navbar.tsx
+++ components/landing_page_components/Navbar.tsx
@@ -22,7 +22,7 @@ function Navar() {
             </div>
             <div className={`${!isMenuOpen && "hidden"} md:hidden w-[50%] text-left fixed h-screen flex flex-col border border-l-0 border-y-0 border-r-gray-600 bg-[#121212] top-0 left-0`}>
                 <a href="/"  className="p-4 mb-8 cursor-pointer flex items-center">
-                    <div className="w-10 h-10">
+                    <div className="w-10 h-10 relative">
                         <Image fill src="/logo.png" alt="Dhania logo" />
                     </div>
                     <div className="uppercase text-2xl">Dhania.Ai</div>
